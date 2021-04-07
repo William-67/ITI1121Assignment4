0e064cf5808a355e81964c386456c8bb01992797
lixiong wei <lwei075@uottawa.ca>
2021-04-07 17:53:35 -0400

    A4

---
+++ StudentInfo.java
@@ -0,0 +1,28 @@
+/**
+ * Contains a method <code>void display()</code> that all the <code>main</code>
+ * methods call to show the student information. Fill the box with your personal
+ * information.
+ *
+ * @author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+ */
+
+public class StudentInfo {
+
+	/**
+	 * Displays the student information: student name, id, section, etc for each
+	 * member of the team.
+	 */
+
+	public static void display() {
+
+		System.out.println("************************************************************");
+		System.out.println("*                    Lixiong Wei                           *");
+		System.out.println("*                     300145970                            *");
+		System.out.println("*                    Vincent Tan                           *");
+		System.out.println("*                     300125812                            *");
+		System.out.println("************************************************************");
+		System.out.println();
+
+	}
+
+}
